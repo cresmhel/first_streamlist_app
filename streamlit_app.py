38054d0f892c01c8954a01572081f04a7e003c7a
cresmhel <136776716+cresmhel@users.noreply.github.com>
2023-06-16 13:22:52 +0800

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.Header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('mega 3 & Blue Berry Oatfmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range Egg')
