f6acf3dc1518ec0858000e71dbf08fe581b61e63
cresmhel <136776716+cresmhel@users.noreply.github.com>
2023-06-16 13:29:27 +0800

    Update streamlit_app.py
    fixed typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blue Berry Oatfmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free Range Egg')
-streamlist.text('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
